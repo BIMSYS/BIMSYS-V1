d3b1b0a9f848c896df8005c38c2fb1021bbe4318
akbaraffaruk <akbaraffaruk13@gmail.com>
2021-01-03 15:05:22 +0700

    Admin Lesson Update Resolved

--- app/Http/Controllers/LessonController.php
+++ app/Http/Controllers/LessonController.php
@@ -71,9 +71,8 @@ class LessonController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Lesson $lesson)
     {
-        //
     }
 
     /**
@@ -82,9 +81,13 @@ class LessonController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Lesson $lesson)
     {
-        //
+        $lesson = $lesson::where('id', $lesson->id)->firstOrFail();
+
+        return view('admin.lesson.update', [
+            'lesson' => $lesson
+        ]);
     }
 
     /**
@@ -94,9 +97,29 @@ class LessonController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Lesson $lesson)
     {
-        //
+        $request->validate([
+            'lesson_name' => 'required|string|max:255',
+            'lesson_code' => 'required|string|max:3|min:3|alpha_dash|unique:lessons,lesson_code',
+            'lesson_description' => 'required|string|max:255'
+        ]);
+
+        // update lesson
+        $lesson->lesson_name = ucwords(Str::lower($request['lesson_name']));
+        $lesson->lesson_code = Str::upper($request['lesson_code']);
+        $lesson->lesson_description = $request['lesson_description'];
+        $lesson->lesson_enroll = Str::random(6);
+
+        // save lesson
+        $lesson->save();
+
+        // message
+        if ($lesson) {
+            return redirect(route('admin.lesson.index'))->with('success', 'Lesson berhasil diupdate');
+        } else {
+            return redirect(route('admin.lesson.create'))->with('danger', 'Lesson gagal diupdate!');
+        }
     }
 
     /**
---
+++ resources/views/admin/lesson/update.blade.php
@@ -0,0 +1,110 @@
+@extends('layouts.contentLayout', ['title' => 'Update Lesson'])
+
+@section('content')
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+                    <li class="breadcrumb-item"><a href="{{ route('admin.lesson.index') }}">Lesson</a></li>
+                    <li class="breadcrumb-item active">Update Lesson</li>
+                </ol>
+            </div>
+        </div>
+    </div><!-- /.container-fluid -->
+</section>
+
+<section class="content mt-5">
+    <div class="col-md-12 d-flex justify-content-center">
+        <div class="card" style="width: 50rem;">
+            <div class="card-header bg-primary">
+                <h1 style="text-align:center;">Update Lesson</h1>
+            </div>
+
+            <div class="card-body">
+                <fieldset>
+                    <form method="POST" action="{{ route('admin.lesson.update', $lesson) }}">
+                        @csrf
+                        @method('PATCH')
+                        <div class="form-group ">
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text bg-primary">
+                                        <span class="fas fa-book"></span>
+                                    </div>
+                                </div>
+
+                                <input type="text" name="lesson_name"
+                                    class="form-control @error('lesson_name') is-invalid @enderror" name="lesson_name"
+                                    placeholder="Lesson Name" value="{{ $lesson->lesson_name }}"
+                                    autocomplete="lesson_name" autofocus>
+                                @error('lesson_name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group ">
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text bg-primary">
+                                        <span class="fas fa-id-card-alt"></span>
+                                    </div>
+                                </div>
+
+                                <input type="text" name="lesson_code"
+                                    class="form-control @error('lesson_code') is-invalid @enderror"
+                                    placeholder="Lesson Code" value="{{ $lesson->lesson_code }}"
+                                    autocomplete="lesson_code" autofocus>
+                                @error('lesson_code')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group ">
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text bg-primary">
+                                        <span class="fas fa-quote-right"></span>
+                                    </div>
+                                </div>
+
+                                <input type="text" name="lesson_description"
+                                    class="form-control @error('lesson_description') is-invalid @enderror"
+                                    name="lesson_description" placeholder="Lesson Description"
+                                    value="{{ $lesson->lesson_description }}" autocomplete="lesson_description" autofocus>
+                                @error('lesson_description')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div>
+                            <ul class="nav justify-content-center">
+                                <button type="submit" class="border-0 bg-transparent"><img
+                                        src="{{ URL::asset('/img/check.png') }}" style="width: 50px; height: 50px;"
+                                        class="mb-2 mr-3 mt-3" alt="Submit"></button>
+                                <button type="reset" class="border-0 bg-transparent"><img
+                                        src="{{ URL::asset('/img/cancel.png') }}" style="width: 50px; height: 50px;"
+                                        class="mb-2 mr-3 mt-3" alt="Cancel"></button>
+                            </ul>
+                        </div>
+                    </form>
+                </fieldset>
+            </div> <!-- tutup card body -->
+        </div> <!-- tutup card -->
+    </div> <!-- tutup col -->
+
+</section>
+@endsection
